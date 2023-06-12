43ca2bcbb02b59153f6e7889dbfd1a0924f075d5
Luy_ <choyanluy@gmail.com>
2023-06-12 20:42:34 -0300

    primeiras informaçoes

---
+++ java.js
@@ -0,0 +1,27 @@
+
+
+
+function calcular() {
+
+    let antes1 = document.getElementById('antes')
+    let depois2 = document.getElementById('depois')
+
+    let A = Number(antes1.value)
+    let D = Number(depois2.value)
+
+    if (A = 0) {
+        alert ('Error')
+    } else {
+        alert ('Tudo certo')
+    }
+
+}
+
+
+
+/*let A = 100
+let D = 59
+
+let resultado = ((A - D) / A) * 100
+
+console.log(resultado + '%')*/
