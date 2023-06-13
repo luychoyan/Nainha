19c569a8301105802a76992b0d2c1b5dfe15b381
Luy_ <choyanluy@gmail.com>
2023-06-12 23:06:07 -0300

    mais um calculo

--- java.js
+++ java.js
@@ -4,7 +4,7 @@ function calcular() {
 
     let antes = document.getElementById('antes1')
     let depois = document.getElementById('depois2')
-    let resultado = document.getElementById('resposta')
+    let resultado = document.getElementById('resposta1')
 
     
 
@@ -24,6 +24,51 @@ function calcular() {
 
     }
 
+}
+
+function calcular2() {  //Segunda formula
+
+    let peso = document.getElementById('peso2')
+    let resposta = document.getElementById('resposta2')
+
+    if (peso.value.length <= 0) {
+
+        alert ('Valor Inválido, Preencha todos os Campos!')
+
+    } else {
+
+        let peso_real = Number(peso.value)
+
+        let resultado2 = peso_real * 0.03
+
+        resposta.innerHTML = resultado2 + 'Kg'
+    }
+
+
+
+}
+
+
+function calcular3() {
+
+    let consumodiario = document.getElementById('consumo')
+    let periodo = document.getElementById('periodo')
+    let resposta3 = document.getElementById('resposta3')
+
+    if (consumodiario.value.length <= 0 || periodo.value.length <= 0) {
+
+        alert ('Valor Inválido, Preencha todos os Campos!')
+
+    } else {
+
+        let consumo = Number(consumodiario.value)
+        let periodo = Number(periodo.value)
+
+        let resultado3 = consumo * periodo
+
+        resposta3.innerHTML = resultado3 + 'Kg/ha'
+
+    }
 }
 
 
